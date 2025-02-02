f915dd6c5c75c8a8470bbff9c4ce8ca94d69b032
mouleshleo <mouleshleo06@gmail.com>
2025-02-02 12:34:07 +0530

    ui changed

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,20 @@ function App() {
   };
 
   return (
-    <div className="p-6 w-96 mx-auto border border-slate-200 bg-slate-100 flex rounded-xl flex-col justify-center items-center mt-20">
-      <h1 className="text-9xl font-bold mb-8 tracking-wide">URL Shortener.</h1>
+    <div className="p-6 w-96 mx-auto border  backdrop-blur-md shadow-xl border-slate-200 bg-slate-100/30 flex rounded-xl flex-col justify-center items-center mt-32">
+      <h1 className="text-9xl font-bold mb-8 tracking-wide text-black/80"><span className="text-black/60 mr-7">URL</span><br></br>Shortener.</h1>
 
-      <form onSubmit={handleSubmit} className="space-y-3 rounded-xl">
+      <form onSubmit={handleSubmit} className="space-y-3 rounded-xl p-2">
         <input
           type="text"
           placeholder="Enter URL"
           value={longUrl}
           onChange={(e) => setLongUrl(e.target.value)}
-          className="border p-2 rounded w-full"
+          className="border p-2 rounded w-full bg-transparent"
         />
         <button 
           type="submit" 
-          className="bg-slate-800 text-white p-2 rounded w-full">
+          className="bg-slate-700/70 shadow-lg mt-2 cursor-pointer transition-all active:translate-y-0.5 text-white p-2 rounded w-full">
           Shorten URL
         </button>
       </form>
@@ -35,7 +35,7 @@ function App() {
 
       {shortUrl && (
         <p className="mt-4">
-          Shortened URL: <a href={shortUrl} target="_blank" rel="noopener noreferrer">{shortUrl}</a>
+          Shortened URL: <a href={shortUrl} className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">{shortUrl}</a>
         </p>
       )}
     </div>
